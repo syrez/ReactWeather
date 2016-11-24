80e2959c3a8c209e4ddace6e372ca6c5f339bb95
syrez <vinkeey@gmail.com>
2016-11-24 16:00:53 +0100

    update server.js

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
-	if(req.headers['x-forwarded-proto'] == 'https'){
+	if(req.headers['x-forwarded-proto'] === 'https'){
 		res.redirect('http://' + req.hostname + req.url)
 	}else{
 		next();
 	}
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function(req, res) {
   res.sendFile('index.html', { root: __dirname + '/public' });
